5f988b33ff1b75b89f9135e662df4288d93d5561
prasanthparaselli-rally <78722198+prasanthparaselli-rally@users.noreply.github.com>
2022-02-01 20:39:43 +0530

    2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,14 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import cakeReducer from "./cake/cakeReducer";
 import rootReducer from "./rootReducer";
 
 //const store = createStore(cakeReducer);
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, thunk))
+);
 
 export default store;
